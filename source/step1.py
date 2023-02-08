3ec1f079051e54530152b3879a49ff5026e7b0e4
Brad Neil <brad@friedorange.xyz>
2023-02-08 20:10:31 +1100

    Generate temporary font with drawn glyphs

--- source/step1.py
+++ source/step1.py
@@ -2,8 +2,28 @@ import fontforge
 import os
 
 NAME_LIST = "quikscript.nam"
-GLYPH_FOLDER = "temp"
+GLYPH_FOLDER = "temp\\outlines"
+TEMP_FOLDER = "temp"
+EM_SIZE = 1000
+
+
+qs_unicodes = {}
+with open(NAME_LIST) as name_list:
+    for line in name_list:
+        unicode, name = line.strip().split(" ")
+        qs_unicodes[name] = int(unicode, 16)
+        
+print(qs_unicodes)
+
+font = fontforge.font()
+font.encoding = "UnicodeFull"
+font.em = EM_SIZE
 
 source_files = os.listdir(GLYPH_FOLDER)
-   
-print(source_files)
+for source in source_files:
+    name = source[:-4]
+    print(name)
+    font.createChar(qs_unicodes[name] if name in qs_unicodes else fontforge.unicodeFromName(name), name)
+    font[name].importOutlines(GLYPH_FOLDER + "\\" + source, scale=False)
+
+font.save(TEMP_FOLDER + "\\step1.sfd")
--- step1.bat
+++ step1.bat
@@ -1,3 +1,4 @@
 @cd source
 "process svg.bat" | inkscape --shell
+fontforge -script step1.py
 @cd ..
